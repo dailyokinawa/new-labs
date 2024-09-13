dcdbb49029ce37199570f10d203e2a27398168cb
dailyokinawa <150796995+dailyokinawa@users.noreply.github.com>
2024-09-13 15:35:28 +0300

    Add files via upload

--- 1 лабораторная/Задание 3 (жесткий колхоз)/index.js
+++ 1 лабораторная/Задание 3 (жесткий колхоз)/index.js
@@ -4,20 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
-// Данные для задач
 let tasks = [];
 
-// Главная страница с задачами
 app.get('/', (req, res) => {
     res.render('index', { tasks });
 });
 
-// Добавление новой задачи
 app.post('/add-task', (req, res) => {
     const newTask = req.body.task;
     if (newTask) {
@@ -26,21 +22,18 @@ app.post('/add-task', (req, res) => {
     res.redirect('/');
 });
 
-// Удаление задачи
 app.post('/delete-task/:index', (req, res) => {
     const index = req.params.index;
     tasks.splice(index, 1);
     res.redirect('/');
 });
 
-// Завершение задачи
 app.post('/complete-task/:index', (req, res) => {
     const index = req.params.index;
     tasks[index].completed = !tasks[index].completed;
     res.redirect('/');
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`); // Исправлено здесь
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
